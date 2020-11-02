909b784ca31688461f7b39013e21a7fb88af705d
张梦雨 <1083874521@qq.com>
2020-11-02 14:49:28 +0800

    添加

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App'
-import api from './common/api.js'
+import master from './common/api.js'
 
 Vue.config.productionTip = false
 Vue.prototype.$api = api;
 App.mpType = 'app'
 
 
-// 我再master 分支下，提交的文件
+
 
 
 
